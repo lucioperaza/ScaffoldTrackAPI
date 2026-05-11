4986951bd8b08cc1ac4c3b28e00e79a79f7c8cd6
lperaza2 <lperaza2@stu.southtexascollege.edu>
2026-05-11 13:12:47 -0500

    added cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
+
 import auth from "./routes/auth";
 import scaffolds from "./routes/scaffolds";
 
 const app = new Hono();
+
+app.use(
+  "*",
+  cors({
+    origin: "*",
+    allowMethods: ["GET", "POST", "PUT", "DELETE"],
+    allowHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.get("/", (c) => {
   return c.json({
